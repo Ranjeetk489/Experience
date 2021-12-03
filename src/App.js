dbb5f54f152b648d56bda9f53c35dbe5eaf26ab2
Ranjeetk489 <49064698+Ranjeetk489@users.noreply.github.com>
2021-12-04 01:11:24 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tabs-project";
+
+function App() {
+  const [details, setDetails] = useState([]);
+  const [value, setValue] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchJobs = async () => {
+    try {
+      const jobs = await fetch(url);
+      const jobsData = await jobs.json();
+      setDetails(jobsData);
+      setIsLoading(false);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  console.log(details);
+  if (isLoading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+  const { company, dates, duties, title } = details[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {details.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div className="job-desc" key={index}>
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
+}
+
+export default App;
